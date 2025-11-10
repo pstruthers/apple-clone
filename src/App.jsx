1bcd52291c90ff0d86425a268b509366c5f41726
Paige <paige.struthers@gmail.com>
2025-11-10 13:51:08 -0600

    3D product viewer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
-import Hero from "./components/Hero"
-import Navbar from "./components/Navbar"
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import ProductViewer from "./components/ProductViewer";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const App = () => {
-	return (
-		<main>
-			<Navbar />
-			<Hero />
-		</main>
-	)
-}
+  return (
+    <main>
+      <Navbar />
+      <Hero />
+      <ProductViewer />
+    </main>
+  );
+};
 
-export default App
+export default App;
